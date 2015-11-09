8145f449d3621b65071b351d715b0c6a56ae36db
jbentley6 <jasen.bentley@sage.com>
2015-11-09 14:28:10 -0500

    hoping to get better errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-  
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
